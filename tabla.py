b3e8fb17006715b7d5b10d46da004be15da755f4
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-09-23 17:00:03 +0200

    Add files via upload

---
+++ tabla.py
@@ -0,0 +1,16 @@
+from logging import raiseExceptions
+
+
+x = int(input("Introduzca número de filas: "))
+y = int(input("Introduzca número de columnas: "))
+
+if x <1 or x>9:
+    raise Exception("El número x tiene que estar contenido entre 1 y 9")
+
+if y <1 or y>9:
+    raise Exception("El número y tiene que estar contenido entre 1 y 9")
+
+
+for x1 in range(x):
+    for y1 in range(y):
+        print(" * ", end='')
